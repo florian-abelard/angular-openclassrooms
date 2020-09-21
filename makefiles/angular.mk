2f6df7be17062dbd062520a501bb4a362cf6bf7b
Florian Abélard <f.abelard@gmail.com>
2020-09-21 16:53:06 +0200

    chore(docker): run ng serve from the Dockerfile

--- makefiles/angular.mk
+++ makefiles/angular.mk
@@ -2,12 +2,12 @@
 # Angular cli Makefile
 #------------------------------------------------------------------------------
 
-ANGULAR_DOCKER_CMD = docker-compose -f ${DOCKER_COMPOSE_FILE} exec -T --user ${USER_ID}:${GROUP_ID} angular ${1}
+ANGULAR_DOCKER_CMD = docker-compose -f ${DOCKER_COMPOSE_FILE} run --rm -T --user ${USER_ID}:${GROUP_ID} angular ${1}
 
 #------------------------------------------------------------------------------
 
-angular-serve: ##@angular start angular server
-	$(call ANGULAR_DOCKER_CMD, ng serve --host 0.0.0.0)
+angular-test: ##@angular start angular server
+	$(call ANGULAR_DOCKER_CMD, ls -l)
 
 #------------------------------------------------------------------------------
 
