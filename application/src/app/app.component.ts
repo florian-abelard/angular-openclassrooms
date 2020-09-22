f4468ddd10df92ab3af995c7aa7123a7c9098d64
Florian Abélard <f.abelard@gmail.com>
2020-09-22 15:38:11 +0200

    refactor(blog): fetch blog posts

--- application/src/app/app.component.ts
+++ application/src/app/app.component.ts
@@ -8,27 +8,36 @@ import { Post } from './model/post';
 })
 export class AppComponent {
 
-  posts: Array<Post> = [];
+  posts: Post[] = [];
 
   constructor() {
 
-    this.posts.push(
+    this.posts = this.fetchBlogPosts();
+  }
+
+  fetchBlogPosts(): Post[] {
+
+    let posts: Post[] = [];
+
+    posts.push(
       new Post(
         '8 habitudes de développeur(euse)s qui font progresser',
         'Avec le temps, j\'ai fini par comprendre que ce qui comptait le plus dans ce métier, c\'était les habitudes. Toutes ces petites décisions et façons de faire.'
       )
     );
-    this.posts.push(
+    posts.push(
       new Post(
         'Faut-il être passionné(e) pour être développeur(euse) ?',
         'La passion chez les développeurs est un concept qui met une pression folle à beaucoup de gens. Tu en entends parler en permanence.'
       )
     );
-    this.posts.push(
+    posts.push(
       new Post(
         'L\'incroyable ego des développeur(euse)s',
         'L\'ego des développeurs est incroyable. J\'entends beaucoup parler du syndrome de l\'imposteur mais le syndrome de l\'énorme boulard est tout aussi présent.'
       )
     );
+
+    return posts;
   }
 }
