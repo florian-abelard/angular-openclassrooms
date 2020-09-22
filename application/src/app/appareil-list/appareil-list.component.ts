3badeb339a5009a83db97b3617dd2c4bf0f7c5dd
Florian Abélard <f.abelard@gmail.com>
2020-09-22 11:09:15 +0200

    refactor: create appareil list component

---
+++ application/src/app/appareil-list/appareil-list.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appareil-list',
+  templateUrl: './appareil-list.component.html',
+  styleUrls: ['./appareil-list.component.scss']
+})
+export class AppareilListComponent {
+
+  isAuthenticated = false;
+
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+
+  appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      name: 'Frigo',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint'
+    }
+  ];
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuthenticated = true;
+      }, 4000
+    );
+  }
+
+  onAllumer(): void {
+    console.log('On alllume tous les appareils');
+  }
+}
