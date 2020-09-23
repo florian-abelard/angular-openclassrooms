774e54d37ece31d509bde10f7642b02336e09b68
Florian Abélard <f.abelard@gmail.com>
2020-09-23 09:47:17 +0200

    feat: setting up routing

--- application/src/app/app-routing.module.ts
+++ application/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { AuthComponent } from './auth/auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'appareils', component: AppareilViewComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: '', component: AppareilViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostComponent } from './post/post.component';
 import { AppareilService } from './services/appareil.service';
